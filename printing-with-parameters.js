aca6f8a70f8825ec5759dbc1142af925cf604c34
Sungil Saul Lee <jangsoo86@gmail.com>
2019-04-08 18:56:01 +0000

    Step 3

---
+++ printing-with-parameters.js
@@ -0,0 +1,22 @@
+function getAndPrintHTML (options) {
+
+  /* Add your code here */
+var https = require('https');
+https.get (options, function(response) {
+  var bufferedData = '';
+  response.on('data', function(data){
+    bufferedData += data;
+    console.log(bufferedData);
+  })
+})
+
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+// console.log(requestOptions.host + requestOptions.path);
+
+getAndPrintHTML(requestOptions);
